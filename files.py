4a9af63d9c7e7cff8ab1da8ec858feba70c3095d
DrZlo13 <who.just.the.doctor@gmail.com>
2021-08-15 01:58:58 +1000

    misc improvements

--- files.py
+++ files.py
@@ -47,22 +47,27 @@ class FlipperStorage:
 
     def start(self):
         self.port.open()
-        self.send(self.CLI_SOH)
-        self.read.until(self.CLI_PROMPT)
+        self.send_and_wait_prompt(self.CLI_SOH)
 
     def stop(self):
         self.port.close()
 
     def send(self, line):
         self.port.write(line.encode())
-        #print(line.encode(), end = '')
 
-    def list_tree(self, start = "/", level = 0):
-        start = start.replace('//', '/')
-
-        self.send('storage list ' + start +  '\r')
-
+    def send_and_wait_eol(self, line):
+        self.send(line)
         self.read.until(self.CLI_EOL)
+
+    def send_and_wait_prompt(self, line):
+        self.send(line)
+        self.read.until(self.CLI_PROMPT)
+
+    def list_tree(self, path = "/", level = 0):
+        path = path.replace('//', '/')
+
+        self.send_and_wait_eol('storage list "' + path +  '"\r')
+
         data = self.read.until(self.CLI_PROMPT)
         data = data.split(b'\r\n')
 
@@ -92,35 +97,36 @@ class FlipperStorage:
             line = line.split(" ", 1)
             if(line[0] == '[D]'):
                 print('/' + line[1])
-                self.list_tree(start + '/' + line[1], level + 1)
+                self.list_tree(path + '/' + line[1], level + 1)
             else:
                 line = line[1].rsplit(" ", 1)
                 print(line[0] + ', size ' + line[1])
 
     def send_file(self, filename_from, filename_to):
-        self.send('storage remove ' + filename_to +  '\r')
-        self.read.until(self.CLI_PROMPT)
+        self.send_and_wait_prompt('storage remove "' + filename_to +  '"\r')
 
-        file = open(filename_from, 'rb+')
+        file = open(filename_from, 'rb')
         filesize = os.fstat(file.fileno()).st_size
 
         BUFFER_SIZE = 512
         while True:
             filedata = file.read(BUFFER_SIZE)
-            if(len(filedata) == 0):
+            size = len(filedata)
+            if(size == 0):
                 break
-            self.send('storage write ' + filename_to +  ' ' + str(len(filedata)) + '\r')
-            self.read.until(self.CLI_EOL)
+            self.send_and_wait_eol('storage write "' + filename_to +  '" ' + str(size) + '\r')
             self.port.write(filedata)
+            self.port.read(size)
             self.read.until(self.CLI_PROMPT)
-            print(str(round(file.tell() / filesize * 100)) + '%')
+            print(str(round(file.tell() / filesize * 100)) + '%', end='\r')
         file.close()
+        print()
 
     def read_file(self, filename):
-        self.send('storage read ' + filename + '\r')
-        self.read.until(self.CLI_EOL)
+        self.send_and_wait_eol('storage read "' + filename + '"\r')
         size = self.read.until(self.CLI_EOL)
         size = int(size.split(b': ')[1])
+        # TODO read in chunks
         filedata = self.port.read(size)
         self.read.until(self.CLI_PROMPT)
         return filedata
@@ -130,16 +136,20 @@ class FlipperStorage:
         file.write(self.read_file(filename_from))
         file.close()
 
+FILE_HERE = 'tv_ir.txt'
+FILE_HERE_TMP = 'tv_ir.txt.tmp'
+FILE_ON_FLIPPER = '/ext/tv_ir.txt'
+
 file = FlipperStorage('COM16')
 file.start()
-file.send_file('test.file', '/ext/test.file')
-file.receive_file('/ext/test.file', 'test2.file')
+file.send_file(FILE_HERE, FILE_ON_FLIPPER)
+file.receive_file(FILE_ON_FLIPPER, FILE_HERE_TMP)
 
-# if(filecmp.cmp('test.file', 'test2.file', False)):
-#     print('OK')
-# else:
-#     print('Error')
-#os.remove("test2.file")
+if(filecmp.cmp(FILE_HERE, FILE_HERE_TMP, False)):
+    print('OK')
+else:
+    print('Error')
+os.remove(FILE_HERE_TMP)
 
 file.list_tree()
 file.stop()
